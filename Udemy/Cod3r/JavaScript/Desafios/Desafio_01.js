fa07196b3163d5a19f207a39012daa315946f701
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2022-12-27 11:41:12 -0300

    Estudos HTML5 e CSS3

---
+++ Udemy/Cod3r/JavaScript/Desafios/Desafio_01.js
@@ -0,0 +1,31 @@
+/*
+Executar:
+    // Bloco Par
+    2
+    4
+    6
+    8
+    10
+
+    // Bloco ímpar
+    1
+    3
+    5
+    7
+    9
+*/
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
+
+{
+    console.log('\n1');
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
---
+++ Udemy/Cod3r/JavaScript/Exercicios/primeiro.js
@@ -0,0 +1,26 @@
+// Comentario padrao em Js
+
+{
+    {
+        console.log('Similar ao print');
+        {
+            console.log('Bloco dentro de outro bloco de código')
+        }
+    }
+}
+
+
+/* 
+Essa é uma forma de colocar um comentário extenso no Js
+{
+    Bloco de código;
+}
+*/
+
+{
+    {
+        console
+        .
+        log(2 + 'x =', 4);
+    }
+}
